761868fc814db0beba2530e73b8a6e90d6c28402
rshuee <140012264+rshuee@users.noreply.github.com>
2023-07-19 15:20:06 -0400

    Add files via upload

---
+++ Score calculator.py
@@ -0,0 +1,17 @@
+title=(input('Welcome, we will help you calculate your scores.'))
+name=(input('Enter your name: '))
+title2=(input('Lets start'))
+
+score1=int(input('Enter your first score: '))
+score2=int(input('Enter second score: '))
+score3=int(input('Enter the third score: '))
+total=score1+score2+score3
+print('Your total score is',total)
+
+title3=(input('Press enter to find your percentage'))
+
+per=total/300*100
+print(per)
+
+title4=(input('You have reached the end'))
+
